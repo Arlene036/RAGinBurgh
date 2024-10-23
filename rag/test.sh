eed306b8141ad2fd43c38d02b664b92868159fb4
Yaqi Wang <614223070@qq.com>
2024-10-22 21:32:26 -0400

    change backbone model

--- rag/test.sh
+++ rag/test.sh
@@ -1,6 +1,7 @@
 device="cuda:0"
 show_progress_bar="--show_progress_bar"
 normalize_embeddings="--normalize_embeddings"
+compression_retriever=“--compression_retriever”
 directory_path="../data_collect/raw_documents"
 query_file="../QA/test_questions.csv"
 max_new_tokens_values=(128)
@@ -17,8 +18,11 @@ for max_new_tokens in "${max_new_tokens_values[@]}"; do
 
       python rag.py \
           --device $device \
+          --generator "mistralai/Mistral-7B-Instruct-v0.2" \
+          --faiss_output_dir faiss_index_10221510 \ 
           $show_progress_bar \
           $normalize_embeddings \
+          $compression_retriever \
           --query_file $query_file \
           --max_new_tokens $max_new_tokens \
           --generator_batch_size $generator_batch_size \
